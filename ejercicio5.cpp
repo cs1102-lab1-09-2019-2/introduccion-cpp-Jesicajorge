1f689ffdcbfb28c9d660f03f298c41b314e9fd4b
Jesicajorge <jessica.jorge@utec.edu.pe>
2019-08-25 18:31:42 -0500

    ejercicio5 arreglado

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
 using std::cout;
 
 int main() {
 
-    vector<int> a {1, 2, 3, 4, 5};
-    for (auto i : a) {
+    int a []= {1, 2, 3, 4, 5};
+    for (auto i=1;i<=a[4];i++) {
+        cout<<i;
         // Imprimir los numeros
     }
+    return 0;
 }
