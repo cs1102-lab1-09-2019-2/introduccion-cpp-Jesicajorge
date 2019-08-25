2d3f1abae21e4f6ce00f709153375fff677375a3
Jesicajorge <jessica.jorge@utec.edu.pe>
2019-08-25 17:54:44 -0500

    ejercicio3 arreglado

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using std::cout;
-
 int main() {
-    for (int i = 0; i < ; i++) {
+    for (int i = 0; i <10; i++) {
+        cout<<i;
         //imprimir los 10 numeros [0-9]
     }
 }
