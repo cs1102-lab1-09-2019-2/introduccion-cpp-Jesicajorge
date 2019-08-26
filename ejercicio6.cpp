716c2ae43019dfd11f363d2dd20ca1353a3aaadd
Jesicajorge <jessica.jorge@utec.edu.pe>
2019-08-25 19:06:52 -0500

    ejercicio6 arreglado

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,13 +1,17 @@
-
+#include<iostream>
+#include<vector>
+using namespace std;
 
 int main() {
     vector<vector<int>> v{{1, 2}, {3, 4}, {5, 6}};
-    for (auto a : v) {
-        for (auto i : a) {
+    for (auto a=0;a<=3;a++) {
+        for (auto i =1;i<=a;i++){
+            cout<<i;
             // imprimir i
         }
-        // salto de linea
+        cout<<endl;// salto de linea
     }
+    return 0;
 }
 
 
