91d43bd9b39ad21b978e86e8fa838270c5a197ba
Jesicajorge <jessica.jorge@utec.edu.pe>
2019-08-25 19:53:40 -0500

    ejercicio9 arreglado

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -4,10 +4,13 @@ using std::cout;
 int main() {
 
     int i = 1;
-    while (i <= 10) {
-        if (   ) {
+    while (i<=10) {
+        if (i % 2 == 0) {
+            cout << i;
+            i++;
             //imprimir numeros pares verticalmente
-        }
 
+        }
     }
+    return 0;
 }
